a12ab29dde3017a8cf6339bfb2db564898baab6f
Liapustin Maksim <maksim.liapustin@gmail.com>
2016-10-18 20:44:35 +0500

    написал первые тесты на начальную функциональность

---
+++ TagsCloudVisualization/CircularCloudLayouter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using NUnit.Framework;
+
+namespace TagsCloudVisualization
+{
+    public class CircularCloudLayouter
+    {
+        public const int Width = 100;
+        public const int Height = 100;
+
+        private Point centerPoint;
+        public Point CenterPoint
+        {
+            get { return centerPoint; }
+            private set
+            {
+                if (value.X < 0 || value.Y < 0 || value.X > Width || value.Y > Height) throw new ArgumentException();
+                centerPoint = value;
+            }
+        }
+
+        public List<Rectangle> Rectangles { get; set; }
+
+        public CircularCloudLayouter(Point center)
+        {
+            Rectangles = new List<Rectangle>();
+            CenterPoint = center;            
+        }
+
+        public Rectangle PutNextRectangle(Size rectangleSize)
+        {
+            var rectangle = new Rectangle(centerPoint, rectangleSize);           
+            Rectangles.Add(rectangle);
+            return rectangle;
+        }
+
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ TagsCloudVisualization/CircularCloudLayouter_should.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Drawing;
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace TagsCloudVisualization
+{
+    public class CircularCloudLayouter_should
+    {
+        [Test]
+        public void BeEmpty_AfterCreation()
+        {
+            var center = new Point(5, 5);
+            var cloud = new CircularCloudLayouter(center);
+            cloud.Rectangles.Should().BeEmpty();
+        }
+
+
+        
+        [TestCase(-1, 0, TestName = "negative x")]
+        [TestCase(0, -1, TestName = "negative y")]
+        [TestCase(-1, -1, TestName = "negative x and negative y")]
+        [TestCase(CircularCloudLayouter.Width+1, 0, TestName = "x is greater than clouds width")]
+        [TestCase(0, CircularCloudLayouter.Height+1, TestName = "y is greater than clouds height")]
+        public void ThrowException_IfIncorrectCenterPoint(int x, int y)
+        {
+            Assert.Throws<ArgumentException>(() =>
+           {
+               var cloud = new CircularCloudLayouter(new Point(x, y));
+           });
+        }
+
+        [Test]
+        public void PutOneRectangleInCenter()
+        {
+            var center = new Point(10,10);
+            var cloud = new CircularCloudLayouter(center);
+
+            var rectangle = cloud.PutNextRectangle(new Size(5, 5));
+
+            cloud.Rectangles.Should().HaveCount(1);
+            rectangle.Location.ShouldBeEquivalentTo(center);
+        }
+
+    }
+}
