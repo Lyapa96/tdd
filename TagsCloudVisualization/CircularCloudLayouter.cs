43b64a683ea7b5e071f88a0ed0f727f9a5f7159f
Liapustin Maksim <maksim.liapustin@gmail.com>
2016-10-20 19:00:08 +0500

    добавил метод создания спирали

--- TagsCloudVisualization/CircularCloudLayouter.cs
+++ TagsCloudVisualization/CircularCloudLayouter.cs
@@ -10,35 +10,41 @@ namespace TagsCloudVisualization
         public const int Width = 100;
         public const int Height = 100;
 
-        private Point centerPoint;
-        public Point CenterPoint
-        {
-            get { return centerPoint; }
-            private set
-            {
-                if (value.X < 0 || value.Y < 0 || value.X > Width || value.Y > Height) throw new ArgumentException();
-                centerPoint = value;
-            }
-        }
+        public readonly Point CenterPoint;
 
         public List<Rectangle> Rectangles { get; set; }
+        public List<Point> Spiral = new List<Point>();
 
         public CircularCloudLayouter(Point center)
         {
             Rectangles = new List<Rectangle>();
-            CenterPoint = center;            
+            if (center.X < 0 || center.Y < 0 || center.X > Width || center.Y > Height) throw new ArgumentException();
+            CenterPoint = center;
         }
 
         public Rectangle PutNextRectangle(Size rectangleSize)
         {
-            var rectangle = new Rectangle(centerPoint, rectangleSize);           
+            var rectangle = new Rectangle(CenterPoint, rectangleSize);
             Rectangles.Add(rectangle);
             return rectangle;
         }
 
-        public static void Main(string[] args)
+        public void CreateSpiral(double densityOfSpirals, double deltaInDegrees, double numberOfSpirals, Point center)
         {
-            
+            double angleInDegrees = 0; 
+            Spiral.Add(new Point(center.X, center.Y));
+
+            while (angleInDegrees <= (numberOfSpirals*360))
+            {
+                angleInDegrees += deltaInDegrees;
+                var radius = angleInDegrees*densityOfSpirals;
+
+                //Получаем новые значения координат точки в декартовой системе координат:
+                var x = (radius*Math.Cos(angleInDegrees/180*Math.PI)) + center.X;
+                var y = (radius*Math.Sin(angleInDegrees/180*Math.PI)) + center.Y;
+
+                Spiral.Add(new Point((int) x, (int) y));
+            }
         }
     }
 }
--- TagsCloudVisualization/CircularCloudLayouter_should.cs
+++ TagsCloudVisualization/CircularCloudLayouter_should.cs
@@ -8,39 +8,56 @@ namespace TagsCloudVisualization
     public class CircularCloudLayouter_should
     {
         [Test]
-        public void BeEmpty_AfterCreation()
+        public void beEmpty_AfterCreation()
         {
+          
             var center = new Point(5, 5);
             var cloud = new CircularCloudLayouter(center);
             cloud.Rectangles.Should().BeEmpty();
         }
 
-
-        
-        [TestCase(-1, 0, TestName = "negative x")]
-        [TestCase(0, -1, TestName = "negative y")]
-        [TestCase(-1, -1, TestName = "negative x and negative y")]
-        [TestCase(CircularCloudLayouter.Width+1, 0, TestName = "x is greater than clouds width")]
-        [TestCase(0, CircularCloudLayouter.Height+1, TestName = "y is greater than clouds height")]
-        public void ThrowException_IfIncorrectCenterPoint(int x, int y)
-        {
-            Assert.Throws<ArgumentException>(() =>
-           {
-               var cloud = new CircularCloudLayouter(new Point(x, y));
-           });
-        }
-
         [Test]
-        public void PutOneRectangleInCenter()
+        public void putOneRectangleInCenter()
         {
-            var center = new Point(10,10);
+            var center = new Point(10, 10);
             var cloud = new CircularCloudLayouter(center);
-
             var rectangle = cloud.PutNextRectangle(new Size(5, 5));
-
+            
             cloud.Rectangles.Should().HaveCount(1);
             rectangle.Location.ShouldBeEquivalentTo(center);
         }
 
+        [TestCase(-1, 0, TestName = "negative x")]
+        [TestCase(0, -1, TestName = "negative y")]
+        [TestCase(-1, -1, TestName = "negative x and negative y")]
+        [TestCase(CircularCloudLayouter.Width + 1, 0, TestName = "x is greater than clouds width")]
+        [TestCase(0, CircularCloudLayouter.Height + 1, TestName = "y is greater than clouds height")]
+        public void throwException_IfIncorrectCenterPoint(int x, int y)
+        {
+            Assert.Throws<ArgumentException>(() => { var cloud = new CircularCloudLayouter(new Point(x, y)); });
+        }
+
+        [Test]
+        public void putTwoRectangles()
+        {
+            Point center= new Point(50,50);
+            var cloud = new CircularCloudLayouter(center);
+
+            cloud.PutNextRectangle(new Size(10, 10));
+            cloud.PutNextRectangle(new Size(15, 15));
+
+            cloud.Rectangles.Should().HaveCount(2);
+        }
+
+        [Test]
+        public void createSpiral()
+        {
+            Point center = new Point(50, 50);
+            var cloud = new CircularCloudLayouter(center);
+
+            cloud.CreateSpiral(0.05,60,10,center);
+           
+            cloud.Spiral.Should().NotBeEmpty();
+        }
     }
 }
